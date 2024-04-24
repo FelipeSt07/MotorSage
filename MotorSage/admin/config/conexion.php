b1a16e80019209f695de5ba65204d40d33ccdb94
FelipeSt07 <felipesantacruz-2011@hotmail.com>
2024-04-23 22:41:31 -0500

    admin
    primera parte

---
+++ MotorSage/admin/config/conexion.php
@@ -0,0 +1,15 @@
+<?php
+    function conectar() {
+        $hostname = 'localhost';
+        $username = 'root';
+        $password = '';
+        $database = 'db_barberia';
+
+        $conexion = mysqli_connect($hostname,$username,$password,$database);
+        if($conexion->connect_error) {
+            die("Error al conectar la base de datos de la pagina".$conexion->connect_error);
+        }
+        return $conexion;
+    }
+    
+?>
